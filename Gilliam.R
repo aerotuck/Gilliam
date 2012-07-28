85c5f6cd900b9b4fe1e46c85f97d17a38b81c65c
Matthew Krachey <aerotuck@gmail.com>
2012-07-28 06:10:36 -0400

    Running
    A few modifications to the embedded scripts for automating the runs

--- Gilliam.R
+++ Gilliam.R
@@ -11,20 +11,21 @@ library(stringr);library(functional)
 ##################################################################################
 pastes = Curry(paste, sep="")
 run.simout = Curry(run.sim, niter1 = 100000)
-run.simstart = Curry(run.sim, niter1 = 20000, readstart1=FALSE, readstep1 = FALSE)
-chainnumb = pastes('FixKFull', 1:8)
+run.simstart = Curry(run.sim, niter1 = 10000, readstart1=FALSE, readstep1 = FALSE)
+chainnumb = pastes('FixKFull', 1:6)
+
 
 ##################################################################################
 ##################################################################################
 library(multicore)
-mclapply(chainnumb, run.simstart)
-mclapply(chainnumb, run.simout)
+#mclapply(chainnumb, run.simstart)
+#mclapply(chainnumb, run.simout)
 
 ##################################################################################
 ##################################################################################
 run.simout1 = Curry(run.sim, niter1 = 10000)
-for(i in 1:10){ 
-	mclapply(chainnumb, run.simout)
+for(i in 1:2){ 
+	mclapply(chainnumb, run.simout1)
 cat("Done with iteration :", i ,fill=TRUE)
 }
 
